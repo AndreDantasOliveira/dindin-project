b6f2810966dd851aada531aca0dbc61fc684c357
AndreDantasOliveira <51372535+AndreDantasOliveira@users.noreply.github.com>
2021-10-20 20:17:27 -0300

    Update Novo_Curso.vue

--- dindin-education/src/views/Novo_Curso.vue
+++ dindin-education/src/views/Novo_Curso.vue
@@ -1,18 +1,105 @@
 <template>
   <div class="novo">
+    <form id="novo-curso-form" @submit="createCurso">  
     <h1>Novo Curso</h1>
     <div>
-      <b-form-input v-model="text" placeholder="Preencha"></b-form-input>
-      <b-form-input v-model="text" placeholder="Preencha"></b-form-input>
-      <b-form-input v-model="text" placeholder="Preencha"></b-form-input>
-      <b-form-input v-model="text" placeholder="Preencha"></b-form-input>    
+      
+          <div class="input-container">
+              <label for="titulo"></label>
+              <input type="text" id="titulo" name="titulo" v-model="titulo" placeholder="Titulo">
+          </div>
+          <div class="input-container">
+              <label for="capa"></label>
+              <input type="text" id="capa" name="capa" v-model="capa" placeholder="upload capa">
+          </div>
+          <div class="input-container">
+              <label for="prof"></label>
+              <input type="text" id="prof" name="prof" v-model="prof" placeholder="Professor">
+          </div>
+          <div class="input-container">
+              <label for="desc"></label>
+              <input type="text" id="desc" name="desc" v-model="desc" placeholder="Descrição">
+          </div>
+      
   </div>
+  <h2> Aula 1 </h2>
+  <div>
+     
+          <div class="input-container">
+              <label for="titulo_aula"></label>
+              <input type="text" id="titulo_aula" name="titulo_aula" v-model="titulo_aula" placeholder="Titulo da aula">
+          </div>
+          <div class="input-container">
+              <label for="link"></label>
+              <input type="text" id="link" name="link" v-model="link" placeholder="Link da aula">
+          </div>
+          <div class="input-container">
+              <label for="desc_aula"></label>
+              <input type="text" id="desc_aula" name="desc_aula" v-model="desc_aula" placeholder="Descrição da aula">
+          </div>
+         
+  
+  </div>
+  <h2> Aula 2 </h2>
+  <div>
+      
+          <div class="input-container">
+              <label for="titulo_aula"></label>
+              <input type="text" id="titulo_aula" name="titulo_aula" v-model="titulo_aula" placeholder="Titulo da aula">
+          </div>
+          <div class="input-container">
+              <label for="link"></label>
+              <input type="text" id="link" name="link" v-model="link" placeholder="Link da aula">
+          </div>
+          <div class="input-container">
+              <label for="desc_aula"></label>
+              <input type="text" id="desc_aula" name="desc_aula" v-model="desc_aula" placeholder="Descrição da aula">
+          </div>
+         
+   
+  </div>
+  <h2> Aula 3 </h2>
+  <div>
+  
+          <div class="input-container">
+              <label for="titulo_aula"></label>
+              <input type="text" id="titulo_aula" name="titulo_aula" v-model="titulo_aula" placeholder="Titulo da aula">
+          </div>
+          <div class="input-container">
+              <label for="link"></label>
+              <input type="text" id="link" name="link" v-model="link" placeholder="Link da aula">
+          </div>
+          <div class="input-container">
+              <label for="desc_aula"></label>
+              <input type="text" id="desc_aula" name="desc_aula" v-model="desc_aula" placeholder="Descrição da aula">
+          </div>
+         
+     
+  </div>
+  <div class="input-container">
+      <input type="submit" class="submit-btn" value="Salvar">
+  </div>
+  </form>   
   </div>
 </template>
 
 <script>
 export default {
 
+methods:{
+    async createCurso(e) {
+        e.preventDefault();
+
+        const data = {
+            titulo: this.titulo,
+            capa: this.capa,
+            prof: this.prof,
+            desc: this.desc,
+            
+        }
+        
+    }
+}
 }
 </script>
 
@@ -24,5 +111,4 @@ export default {
   margin-top:150px;
   background: rgb(8, 173, 63);
 }
-
 </style>
